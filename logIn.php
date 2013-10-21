c8ded4da5a39991c20464e5c0351d1033eb9d478
vlanie <yellowmoon268@gmail.com>
2013-10-20 23:33:28 -0400

    Create logIn.php

---
+++ logIn.php
@@ -0,0 +1,35 @@
+<?php
+
+//Run a PHP script that connects to Database.
+
+
+include ("account.php") ;
+
+include ("connect.php");
+
+// PHP script that get data from the server/browser
+
+$npi = $_REQUEST ['npi'];
+$taxId = $_REQUEST ['taxId'];
+
+
+//execute select query
+$sql = "select * from providers where NPI = $npi and Tax_ID = $taxId ";
+
+($result = mysql_query($sql)) or die(mysql_error());
+
+$number = mysql_num_rows($result);
+print"<br>number of row " . $number;
+
+if($number > 0 ){
+	
+	//redirect user to assessment tool
+	print"Redirect user to assessment tool";
+	header("refresh:2; url= Assessment.html"); 
+}
+ else {
+    print "Invalid username or password. Try again<br>";
+	echo'<a href="Sign_In.html" alt="back to form html">GO BACK </a>';
+}
+
+?>
