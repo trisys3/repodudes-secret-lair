a9854fff847ee45cf6a42da9f6bcfd74242a6adc
sean@develop.io <sean@develop.io>
2014-01-06 22:26:32 -0500

    added Ayrault as remote repo

---
+++ Konneko/addons/shared_addons/modules/main/views/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<title data-head="title">Loading...</title>
+
+		<meta charset="utf-8" />
+
+		<link rel="stylesheet" type="text/css" href="/main/css/main.css" />
+
+		<script type="text/javascript" src="/main/js/main.js"></script>
+
+	</head>
+
+</html>
