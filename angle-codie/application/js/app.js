fb9ded91cad1d569579960de27bf695bfa4c3cb0
sean@develop.io <sean@develop.io>
2014-03-30 23:19:23 -0400

    removed 2 CodeIgniter-AngularJS projects

--- angle-codie/application/js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module("angle.filters", []);
-
-angular.module("angle.services", []);
-
-var app = angular.module("angle", ['angle.filters', 'angle.services', 'angle.directives']);
--- angle-codie/application/js/services/angle-service.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-angular.module("angle.services", []);
